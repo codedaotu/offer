92aab196b8f05ce789d32057a32c9bbd031178e0
codedaotu <1498530259@qq.com>
2019-08-08 22:32:12 +0800

    判断字符

--- 迭代回溯n个数的排列.cpp
+++ 迭代回溯n个数的排列.cpp
@@ -1,32 +1,11 @@
 #include <iostream>
 using namespace std;
 #include <vector>
-void perm(int list[], int k, int m)
-{
-    if (k == m)//递归的基础部分
-    {
-        for (int i = 0; i <= m; i++)
-        {
-            cout << list[i];
-        }
-        cout << endl;
-    }
-    else
-    {
-        for (int i = k; i <= m; i++)
-        {
-            swap(list[k], list[i]);
-            perm(list, k + 1, m);
-            swap(list[k], list[i]);
-        }
-    }
-}
-
 void perm(vector<int> &arr,int k,int m)
 {
     if(k==m)
     {
-        for(int i=0;i<m;i++)
+        for(int i=0;i<=m;i++)
         {
             cout<<arr[i];
         }
@@ -44,7 +23,7 @@ void perm(vector<int> &arr,int k,int m)
 }
 int main()
 {
-    vector<int> list = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-    perm(list, 0, list.size());
+    vector<int> list = { 1, 2, 3 };
+    perm(list, 0, list.size()-1);
     return 0;
 }
